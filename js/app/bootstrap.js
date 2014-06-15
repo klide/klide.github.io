ac0ba325ca7e250cbaffc2e8e5e93e1152408aaa
Boun Moua <bountahn@gmail.com>
2014-06-15 00:57:07 -0500

    another test

--- js/app/bootstrap.js
+++ js/app/bootstrap.js
@@ -8,6 +8,7 @@ MIDI.loadPlugin({
     // instruments: ['acoustic_grand_piano', 'accordion', 'acoustic_guitar_nylon', 'acoustic_guitar_steel', 'alto_sax', 'overdriven_guitar', 'music_box', 'synth_strings_1', 'violin'],
     callback: function () {
         // Create a new instance of LeafNote() and call init()
+        LeafNote.db = new PouchDb(LeafNote.getDbName());
         var keyboard = new LeafNote.Keyboard();
         keyboard.init();
     }
