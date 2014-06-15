0865bd0dfaf782868f6ee33138021cb98c8ee04a
Boun Moua <bountahn@gmail.com>
2014-06-15 00:02:30 -0500

    Android Local DB fix

--- js/app/main.js
+++ js/app/main.js
@@ -1,87 +1,110 @@
 /**
  * LeafNote Namespace
  */
-var LeafNote = {
+var LeafNote = {};
 
-    // Holds the DB instance
-    db: (typeof(PouchDB('leafNote')) === "undefined") ? new PouchDB('leafNote') : PouchDB('leafNote'),
+/**
+ * Keeps track of the current theme
+ */
+LeafNote.currentTheme = '';
 
-    // Stores the current theme
-    currentTheme: '',
+/**
+ * Determines the localDb name. Will add a prefix of 'websql://' if the client is
+ * an Android client versioin 4.3 and below
+ */
+LeafNote.getDbName = function () {
+    var prefix = '',
+        database = 'leafNote',
+        agent = navigator.userAgent;
 
-    /**
-     * Sets up the Local DB
-     */
-    getDb: function () {
-        if (!this.db) {
-            return new PouchDB('leafNote');
-        } else {
-            return this.db;
+    if (agent.indexOf("Android") >= 0) {
+        var version = parseFloat(agent.slice(agent.indexOf("Android") + 8));
+        if (version <= 4.3) {
+            prefix = 'websql://';
         }
-    },
+    }
+    return prefix + database;
+};
 
-    /**
-     * Applies the currently selected theme
-     * @param {string} themeId The theme id selected
-     */
-    applyTheme: function (themeId) {
-        var self = this,
-            newTheme = themeId || 'theme1',
-            $themeHolderEl = $('#currentTheme'),
-            themeClasses;
+/**
+ * Holds the DB instance
+ */
+LeafNote.db = (typeof(PouchDB(LeafNote.getDbName())) === "undefined") ? new PouchDB(LeafNote.getDbName()) : PouchDB(LeafNote.getDbName());
 
-        // Update the selected theme in localDb
-        this.db.get('theme').then(function (theme) {
-            // If no theme was provided, use the theme from localDb
-            if (!themeId) {
-                newTheme = theme.currentTheme;
-                return $.Deferred().resolve(newTheme).promise();
-            }
-            return self.db.put({'currentTheme': newTheme}, 'theme', theme._rev);
-        }).then(function () {
-            // After saving the selected theme, apply it
-            switch (newTheme) {
-                case 'theme1':
-                    themeClasses = 'theme1';
-                    break;
-                case 'theme2':
-                    themeClasses = 'theme1 theme2';
-                    break;
-                case 'theme3':
-                    themeClasses = 'theme1 theme3';
-                    break;
-                default:
-                    break;
-            }
-            self.currentTheme = newTheme;
-            $themeHolderEl.attr('class', themeClasses);
-            $themeHolderEl.find('body').fadeIn('fast');
-        }, function (err) {
-            // If the theme wasn't found, create the theme and use 'theme1' as a default theme
-            if (err.status == 404) {
-                self.db.put({
-                    _id: 'theme',
-                    currentTheme: newTheme
-                }).then(function () {
-                    self.applyTheme(newTheme);
-                }, function (err) {
-                    alert(err.message);
-                });
-            } else {
-                alert('There was a problem applying the selected theme');
-            }
-        });
-    },
-
-    /**
-     * Gets the current Theme
-     */
-    getCurrentTheme: function () {
-        return this.currentTheme;
+/**
+ * Sets up the Local DB
+ */
+LeafNote.getDb = function () {
+    if (!LeafNote.db) {
+        return new PouchDB(LeafNote.getDbName());
+    } else {
+        return LeafNote.db;
     }
 };
 
-// Setup the DB
+/**
+ * Applies the currently selected theme
+ * @param {string} themeId The theme id selected
+ */
+LeafNote.applyTheme = function (themeId) {
+    var self = LeafNote,
+        newTheme = themeId || 'theme1',
+        $themeHolderEl = $('#currentTheme'),
+        themeClasses;
+
+    // Update the selected theme in localDb
+    this.db.get('theme').then(function (theme) {
+        // If no theme was provided, use the theme from localDb
+        if (!themeId) {
+            newTheme = theme.currentTheme;
+            return $.Deferred().resolve(newTheme).promise();
+        }
+        return self.db.put({'currentTheme': newTheme}, 'theme', theme._rev);
+    }).then(function () {
+        // After saving the selected theme, apply it
+        switch (newTheme) {
+            case 'theme1':
+                themeClasses = 'theme1';
+                break;
+            case 'theme2':
+                themeClasses = 'theme1 theme2';
+                break;
+            case 'theme3':
+                themeClasses = 'theme1 theme3';
+                break;
+            default:
+                break;
+        }
+        self.currentTheme = newTheme;
+        $themeHolderEl.attr('class', themeClasses);
+        $themeHolderEl.find('body').fadeIn('fast');
+    }, function (err) {
+        // If the theme wasn't found, create the theme and use 'theme1' as a default theme
+        if (err.status == 404) {
+            self.db.put({
+                _id: 'theme',
+                currentTheme: newTheme
+            }).then(function () {
+                self.applyTheme(newTheme);
+            }, function (err) {
+                alert(err.message);
+            });
+        } else {
+            alert('There was a problem applying the selected theme');
+        }
+    });
+};
+
+/**
+ * Gets the current Theme
+ */
+LeafNote.getCurrentTheme = function () {
+    return LeafNote.currentTheme;
+};
+
+/**
+ * Get the DB Instance, then Apply the Theme to startup the App
+ */
 LeafNote.getDb().then(function () {
     // Apply the Current theme
     LeafNote.applyTheme();
