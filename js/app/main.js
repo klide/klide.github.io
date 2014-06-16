d8087ad6c9e727d1a4e51b68cf8f27724fa60161
Boun Moua <bountahn@gmail.com>
2014-06-16 00:49:21 -0500

    Added an 'Unsupported Browser' page for browsers that are not supported currently.

--- js/app/main.js
+++ js/app/main.js
@@ -16,17 +16,34 @@ LeafNote.databaseName = 'leafNote';
 /**
  * Checks for browser support
  */
-LeafNote.checkForClientSupport = function () {
-    var agent = navigator.userAgent;
+LeafNote.isSupportedBrowser = function () {
+    var agent = navigator.userAgent,
+        blockedAgents = [{
+            name: 'Internet Explorer',
+            supportedVersion: 10,
+            agentString: 'MSIE'
+        }, {
+            name: 'Android browser',
+            supportedVersion: 4.0,
+            agentString: 'like Gecko) Version'
+        }, {
+            name: 'Opera browser',
+            supportedVersion: 21,
+            agentString: 'Opera'
+        }];
 
-    // Look for the Default Android Browser - It doesn't like WebSQL so it won't support the App
-    if (agent.indexOf("like Gecko) Version") >= 0) {
-        var version = parseFloat(agent.slice(agent.indexOf("like Gecko) Version") + 20));
+    // Display unsupported browser message
+    for (var i=0; i<blockedAgents.length; i++) {
+        if (agent.indexOf(blockedAgents[i].agentString) >= 0) {
+            var versionIndex = agent.lastIndexOf(blockedAgents[i].agentString) + blockedAgents[i].agentString.length + 1,
+                version = parseFloat(agent.slice(versionIndex));
 
-        if (version <= 4.0) {
-            alert('You are using an Android browser which is currently not supported. We hope to support it in the near future. In the meantime, we recommend using the Google Chrome app for Android.');
+            if (version <= blockedAgents[i].supportedVersion) {
+                return false;
+            }
         }
     }
+    return true;
 };
 
 /**
@@ -39,7 +56,7 @@ LeafNote.db = (typeof(PouchDB(LeafNote.databaseName)) === "undefined") ? new Pou
  */
 LeafNote.getDb = function () {
     if (!LeafNote.db) {
-        return new PouchDB(LeafNote.getDbName());
+        return new PouchDB(LeafNote.databaseName);
     } else {
         return LeafNote.db;
     }
@@ -105,11 +122,37 @@ LeafNote.getCurrentTheme = function () {
     return LeafNote.currentTheme;
 };
 
-// Check for Browser Support
-LeafNote.checkForClientSupport();
+// Check for Supported Browsers. If supported, get the DB Instance, then Apply the Theme and startup the App
+if (LeafNote.isSupportedBrowser()) {
+    LeafNote.getDb().then(function () {
+        // Apply the Current theme
+        LeafNote.applyTheme();
 
-// Get the DB Instance, then Apply the Theme to startup the App
-LeafNote.getDb().then(function () {
-    // Apply the Current theme
-    LeafNote.applyTheme();
-});
+        // Load up the MIDI Plugin and then Load the App
+        MIDI.loadPlugin({
+            USE_XHR: false,
+            soundfontUrl: './js/assets/soundfont/',
+            // Commented out Instruments for now... Causes bad Lag in FireFox when it tries to preload
+            // instruments: ['acoustic_grand_piano', 'accordion', 'acoustic_guitar_nylon', 'acoustic_guitar_steel', 'alto_sax', 'overdriven_guitar', 'music_box', 'synth_strings_1', 'violin'],
+            callback: function () {
+                // Create a new instance of LeafNote() and call init()
+                var keyboard = new LeafNote.Keyboard();
+                keyboard.init();
+            }
+        });
+    });
+}
+// If browser isn't supported, display an 'Unsuppported Browser' message
+else {
+    var unsupportedContent = $('<div/>', {
+        html:
+            '<div class="simple-logo"></div>' +
+            '<div class="background">' +
+                '<div><p>You are using a browser which is currently not supported. We hope to support ' +
+                'it in the near future, but in the meantime, we recommend using the latest ' +
+                'version of Google Chrome or FireFox.</p></div>' +
+            '</div>',
+        'class': 'unsupported-browser'
+    });
+    $('html').removeClass('noTheme').attr('class', 'noSupport').find('body').html(unsupportedContent).show();
+}
